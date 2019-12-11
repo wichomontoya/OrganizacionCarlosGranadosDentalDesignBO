12b46ac155ef56d8ece33a2bcaa37121efbea7f1
nixperful <nicolasosorioarias@hotmail.com>
2019-12-11 01:30:40 -0500

    Final implementation of article CRUD

--- src/vue/resource/index.js
+++ src/vue/resource/index.js
@@ -3,9 +3,9 @@ import VueResource from 'vue-resource'
 
 Vue.use(VueResource)
 
-// Vue.http.options.root = 'http://ec2-54-201-251-79.us-west-2.compute.amazonaws.com/api/v1/'
+Vue.http.options.root = 'http://ec2-54-201-251-79.us-west-2.compute.amazonaws.com/api/v1/'
 // Vue.http.options.root = 'http://ec2-54-202-222-69.us-west-2.compute.amazonaws.com/'
-Vue.http.options.root = 'http://localhost:3000/api/v1/'
+// Vue.http.options.root = 'http://localhost:3000/api/v1/'
 Vue.http.interceptor.before = (request, next) => {
   console.log(request)
   next((response) => {
