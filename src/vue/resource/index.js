33d334e40a7742f8dbb1bf101bf9950e96e1a690
anderj amayj <amaander011057@gmail.com>
2019-12-19 09:40:37 -0500

    Change root for request

--- src/vue/resource/index.js
+++ src/vue/resource/index.js
@@ -3,7 +3,7 @@ import VueResource from 'vue-resource'
 
 Vue.use(VueResource)
 
-Vue.http.options.root = 'http://ec2-54-201-251-79.us-west-2.compute.amazonaws.com/api/v1/'
+Vue.http.options.root = 'https://carlosgranadosdentaldesign.com/api/v1/'
 // Vue.http.options.root = 'http://ec2-54-202-222-69.us-west-2.compute.amazonaws.com/'
 // Vue.http.options.root = 'http://localhost:3000/api/v1/'
 Vue.http.interceptor.before = (request, next) => {
